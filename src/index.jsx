749e6dff049a2329f552d8318095394622510580
Mannan <Mannan@DESKTOP-OTIQOF5>
2024-06-29 00:37:42 +0530

    final build for build with delhi with water and weather

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import AirQualityPage from "./pages/AirQualityPage";
-// import WeatherMonitor from "./pages/WeatherMonitor"; // Ensure correct import path
+import WeatherMoniter from "./pages/WeatherMoniter"; // Ensure correct import path
 import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import WaterQuality from "./pages/WaterQuality"; // Corrected typo and path
+import WaterQualiity from "./pages/WaterQualiity"; // Corrected typo and path
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<AirQualityPage />} />
-        {/* <Route path="/water-quality-index" element={<WaterQuality />} />  */}
-        {/* <Route path="/weather2" element={<WeatherMonitor />} /> */}
+        <Route path="/water-quality-index" element={<WaterQualiity />} /> 
+        <Route path="/weather" element={<WeatherMoniter />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
